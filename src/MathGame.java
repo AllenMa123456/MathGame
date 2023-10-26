935b79f4ac45648e6e2d681a57dc84db70e21ecf
Allen <allenma11103@gmail.com>
2023-10-25 22:28:21 -0400

    Added freestyle

--- src/MathGame.java
+++ src/MathGame.java
@@ -1,49 +1,73 @@
 import java.util.Scanner;
 
+
 public class MathGame {
+
 
     private Player player1;
     private Player player2;
+    private Player player3;
+    private Player currentChamp;
+    private Player pastChamp;
     private Player currentPlayer;
     private Player winner;
+    private int streak;
+    private int highscore;
     private boolean gameOver;
     private Scanner scanner;
 
+
     // create MathGame object
-    public MathGame(Player player1, Player player2, Scanner scanner) {
+    public MathGame(Player player1, Player player2, Player player3 , Scanner scanner) {
         this.player1 = player1;
         this.player2 = player2;
+        this.player3 = player3;
         this.scanner = scanner;
         currentPlayer = null; // will get assigned at start of game
         winner = null; // will get assigned when a Player wins
         gameOver = false;
     }
 
-    // ------------ PUBLIC METHODS (to be used by client classes) ------------
+
+// ------------ PUBLIC METHODS (to be used by client classes) ------------
+
 
     // returns winning Player; will be null if neither Player has won yet
     public Player getWinner() {
         return winner;
     }
 
+
+
     // plays a round of the math game
     public void playRound() {
-        chooseStartingPlayer();  // this helper method (shown below) sets currentPlayer to either player1 or player2
+        chooseStartingPlayer(); // this helper method (shown below) sets currentPlayer to either player1 or player2
         while (!gameOver) {
-            printGameState();   // this helper method (shown below) prints the state of the Game
+            printGameState(); // this helper method (shown below) prints the state of the Game
             System.out.println("Current player: " + currentPlayer.getName());
-            boolean correct = askQuestion();  // this helper method (shown below) asks a question and returns T or F
+            boolean correct = askQuestion(); // this helper method (shown below) asks a question and returns T or F
             if (correct) {
                 System.out.println("Correct!");
-                currentPlayer.incrementScore();  // this increments the currentPlayer's score
-                swapPlayers();  // this helper method (shown below) sets currentPlayer to the other Player
+                currentPlayer.incrementScore(); // this increments the currentPlayer's score
+                swapPlayers(); // this helper method (shown below) sets currentPlayer to the other Player
             } else {
                 System.out.println("INCORRECT!");
-                gameOver = true;
-                determineWinner();
+                swapPlayers();
+                printGameState();
+                System.out.println("Current player: " + currentPlayer.getName());
+                correct = askQuestion();
+                if (correct) {
+                    System.out.println("Correct!");
+                    currentPlayer.incrementScore(); // this increments the currentPlayer's score
+                    swapPlayers(); // this helper method (shown below) sets currentPlayer to the other Player
+                } else {
+                    gameOver = true;
+                    determineWinner();
+                }
             }
         }
     }
+
 
     // prints the current scores of the two players
     private void printGameState() {
@@ -51,29 +75,37 @@ public class MathGame {
         System.out.println("Current Scores:");
         System.out.println(player1.getName() + ": " + player1.getScore());
         System.out.println(player2.getName() + ": " + player2.getScore());
+        System.out.println(player3.getName() + ": " + player3.getScore());
         System.out.println("--------------------------------------");
     }
+
 
     // resets the game back to its starting state
     public void resetGame() {
         player1.reset(); // this method resets the player
         player2.reset();
+        player3.reset();
         gameOver = false;
         currentPlayer = null;
         winner = null;
     }
 
-    // ------------ PRIVATE HELPER METHODS (internal use only) ------------
+
+// ------------ PRIVATE HELPER METHODS (internal use only) ------------
+
 
     // randomly chooses one of the Player objects to be the currentPlayer
     private void chooseStartingPlayer() {
-        int randNum = (int) (Math.random() * 2) + 1;
+        int randNum = (int) (Math.random() * 3) + 1;
         if (randNum == 1) {
             currentPlayer = player1;
-        } else {
+        } else if (randNum == 2) {
             currentPlayer = player2;
+        } else {
+            currentPlayer = player3;
         }
     }
+
 
     // asks a math question and returns true if the player answered correctly, false if not
     private boolean askQuestion() {
@@ -94,14 +126,16 @@ public class MathGame {
             num2 = (int) (Math.random() * 10) + 1;
             System.out.print(num1 + " * " + num2 + " = ");
             correctAnswer = num1 * num2;
-        } else {  // option == 4
+        } else { // option == 4
             num2 = (int) (Math.random() * 10) + 1;
             System.out.print(num1 + " / " + num2 + " = ");
             correctAnswer = num1 / num2;
         }
 
+
         int playerAnswer = scanner.nextInt(); // get player's answer using Scanner
         scanner.nextLine(); // clear text buffer after numeric scanner input
+
 
         if (playerAnswer == correctAnswer) {
             return true;
@@ -110,21 +144,52 @@ public class MathGame {
         }
     }
 
+
     // swaps the currentPlayer to the other player
     private void swapPlayers() {
         if (currentPlayer == player1) {
             currentPlayer = player2;
+        } else if (currentPlayer == player2){
+            currentPlayer = player3;
         } else {
             currentPlayer = player1;
         }
     }
 
+
     // sets the winner when the game ends based on the player that missed the question
     private void determineWinner() {
         if (currentPlayer == player1) {
             winner = player2;
+        } else if (currentPlayer == player2) {
+            winner = player3;
         } else {
             winner = player1;
         }
     }
+
+    public int streak() {
+        if (currentChamp != winner){
+            if (pastChamp != winner){
+                currentChamp = winner;
+                streak = 1;
+            }
+        } else {
+            streak += 1;
+        }
+        pastChamp = currentChamp;
+        return streak;
+    }
+    public int highscore() {
+        if (highscore < 1){
+            highscore = 1;
+        } else if (streak > highscore){
+            highscore = streak;
+        } else {
+            highscore += 0;
+        }
+        return highscore;
+
+    }
 }
+
